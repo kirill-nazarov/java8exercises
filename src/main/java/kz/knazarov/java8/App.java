82965b10fc92cc3675a61f81ecd77fd8cb61c4cf
kirill-nazarov <kirill.v.nazarov@gmail.com>
2018-01-21 21:43:47 +0500

    Java 8 initial commit

---
+++ src/main/java/kz/knazarov/java8/App.java
@@ -0,0 +1,27 @@
+package kz.knazarov.java8;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+/**
+ * @author knazarov
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        List l = Arrays.asList("Hello", "World", "My", "List");
+
+        //example of Consumer interface object
+        //Consumer c = (Object o) -> {System.out.println(">>>" + o);};
+        //l.forEach(c);
+
+        //example of forEach method on List
+        //l.forEach((Object o)-> {System.out.println(">>>"+ o);});
+        
+        //the same compact version
+        l.forEach(str->System.out.println(">>>"+ str));
+    }
+}
